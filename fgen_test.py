302176b7141dbee94f7e28db21c9b0273e229cd6
Suyash Kumar <suyash.kumar@duke.edu>
2015-10-13 17:46:11 -0400

    Updated docs with arb waveform generation

--- fgen_test.py
+++ fgen_test.py
@@ -10,8 +10,7 @@ if __name__=="__main__":
     print fgen.getIdn() # Print identity of function generator
     #fgen.pushSin(20)
     #fgen.loadFromMemory("HIFU_SIM") # Loads the stored config "HIFU_SIM"
-    fgen.pushArbitraryWaveform(list(np.linspace(-2000,2000,200,dtype=np.int))
-)
+    fgen.pushArbitraryWaveform(list(np.linspace(-2000,2000,200,dtype=np.int)))
     print fgen.getError() # Gets error off the queue
     print list(np.linspace(-2000,2000,100,dtype=np.int16))
 
