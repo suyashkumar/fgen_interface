71d7fbfde5c66ec04d35827057cec35d0c78483f
Mark Palmeri <mlp6@duke.edu>
2015-12-07 22:19:14 -0500

    add main() for testing

--- fgen_setup_pulse.py
+++ fgen_setup_pulse.py
@@ -2,19 +2,20 @@
 fgen_setup_pulse.py
 """
 from FunctionGenerator import FunctionGenerator
-import numpy as np
-import time
-__author__ = "Peter Hollender (pjh7)" 
+# import numpy as np
+# import time
 
-if __name__=="__main__":
-    fgen=FunctionGenerator(1) #Instantiate function generator
+__author__ = "Peter Hollender (pjh7)"
+
+
+def main():
+    fgen = FunctionGenerator(1)  # Instantiate function generator
     fgen.reset()
-    print fgen.getIdn()
-    fgen.setOutput(1,'OFF')
-    fgen.setOutput(2,'OFF')
+    print(fgen.getIdn())
+    fgen.setOutput(1, 'OFF')
+    fgen.setOutput(2, 'OFF')
     fgen.loadSettings('params.txt')
-    print fgen.getError() # Gets error off the queue
-    del fgen
-    
+    print(fgen.getError())  # Gets error off the queue
 
-
+if __name__ == "__main__":
+    main()
